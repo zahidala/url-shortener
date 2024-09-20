14bbdb5ac8f2f9328bb4b428d4902fcfc4b204aa
Zahid Allaulddin <zahidalaaudin@gmail.com>
2024-09-20 15:23:01 +0300

    WIP: Shorten URL Handler

--- pkg/api/shorten/shorten-url-handler.go
+++ pkg/api/shorten/shorten-url-handler.go
@@ -2,6 +2,8 @@ package shorten
 
 import (
 	"net/http"
+	"url-shortener/pkg/db"
+	"url-shortener/pkg/utils"
 )
 
 func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +13,22 @@ func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	shortenURL := utils.GenerateShortenedURL()
+
+	user := utils.GetUserInfoBySession(w, r)
+
+	query := "INSERT INTO urls (shortUrl, originalUrl, userId) VALUES (?, ?, ?)"
+
+	shortUrlAddExecErr := db.PrepareAndExecute(query,
+		shortenURL,
+		url,
+		user.ID,
+	)
+	if shortUrlAddExecErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }

d07f517e0c54abadee39be59be03f436d4ec3486
Zahid Allaulddin <zahidalaaudin@gmail.com>
2024-09-21 22:38:43 +0300

    fix issues with queries and req.body not being read properly

--- pkg/api/shorten/shorten-url-handler.go
+++ pkg/api/shorten/shorten-url-handler.go
@@ -1,30 +1,42 @@
 package shorten
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
 	"url-shortener/pkg/db"
+	Types "url-shortener/pkg/types"
 	"url-shortener/pkg/utils"
 
 	UrlVerifier "github.com/davidmytton/url-verifier"
 )
 
-func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
-	url := r.FormValue("url")
+type ShortenURLBody struct {
+	URL string `json:"url"`
+}
 
-	if url == "" {
+func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
+	var body ShortenURLBody
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		log.Println("Error decoding body:", err)
+		return
+	}
+
+	if body.URL == "" {
 		http.Error(w, "URL is required", http.StatusBadRequest)
 		return
 	}
 
 	// Ensure the URL starts with http:// or https://
-	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
-		url = "http://" + url
+	if !strings.HasPrefix(body.URL, "http://") && !strings.HasPrefix(body.URL, "https://") {
+		body.URL = "http://" + body.URL
 	}
 
 	verifier := UrlVerifier.NewVerifier()
-	ret, err := verifier.Verify(url)
+	ret, err := verifier.Verify(body.URL)
 
 	if err != nil {
 		log.Println(err)
@@ -40,14 +52,21 @@ func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
 
 	shortenURL := utils.GenerateShortenedURL()
 
-	user := utils.GetUserInfoBySession(w, r)
+	user := utils.GetUserInfoBySession(r)
 
-	query := "INSERT INTO urls (shortUrl, originalUrl, userId) VALUES ($1, $2, $3)"
+	var query string
+	var params []interface{}
+
+	if (user != Types.User{}) {
+		query = "INSERT INTO urls (shortUrl, originalUrl, userId) VALUES ($1, $2, $3)"
+		params = []interface{}{shortenURL, body.URL, user.ID}
+	} else {
+		query = "INSERT INTO urls (shortUrl, originalUrl) VALUES ($1, $2)"
+		params = []interface{}{shortenURL, body.URL}
+	}
 
 	shortUrlAddExecErr := db.PrepareAndExecute(query,
-		shortenURL,
-		url,
-		user.ID,
+		params...,
 	)
 	if shortUrlAddExecErr != nil {
 		log.Println(shortUrlAddExecErr)
@@ -55,5 +74,5 @@ func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	json.NewEncoder(w).Encode(map[string]string{"shortUrl": shortenURL})
 }
